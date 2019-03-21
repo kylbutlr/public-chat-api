6f55bad7ab0c9098dd02b60e99cca86fd6739785
Kyle Butler <kylbutlr@gmail.com>
2019-03-21 11:22:12 -0700

    tests

--- restartTest.sql
+++ restartTest.sql
@@ -4,5 +4,5 @@ TRUNCATE users RESTART IDENTITY;
 ALTER SEQUENCE posts_id_seq RESTART 1;
 ALTER SEQUENCE users_id_seq RESTART 1;
 
-INSERT INTO posts (text, time, date, user_id) VALUES ('test text 1', null, null, 1);
-INSERT INTO users (username, password) VALUES ('user1', 'pass1');
+INSERT INTO posts (text, time, date, user_id) VALUES ('test entry 1', null, null, 1);
+INSERT INTO users (username, password) VALUES ('testUser1', 'pass');
--- test.js
+++ test.js
@@ -19,15 +19,39 @@ afterAll(() => {
   client.end();
 });
 
-describe('GET /posts', () => {
-  it('should return all from /posts', done => {
-    request(app)
-      .get('/posts')
-      .expect(200);
-    db.getPosts((err, res) => {
-      if (err) throw err;
-      expect(res);
-      done();
+describe('posts', () => {
+  const newPost1 = {
+    text: 'test entry 2',
+    time: null,
+    date: new Date(),
+    user_id: 1,
+  };
+
+  describe('GET /posts', () => {
+    it('should return all from /posts', done => {
+      request(app)
+        .get('/posts')
+        .expect(200);
+      db.getPosts((err, res) => {
+        if (err) throw err;
+        expect(res).toHaveLength(1);
+        expect(res[0].user_id).toBe(1);
+        expect(res[0].text).toBe('test entry 1');
+        done();
+      });
+    });
+
+    it('should post to /posts', done => {
+      request(app)
+        .post('/posts')
+        .send(newPost1)
+        .expect(201);
+      db.createPost(newPost1.text, newPost1.time, newPost1.date, newPost1.user_id, (err, res) => {
+        if (err) throw err;
+        expect(res[0].user_id).toBe(1);
+        expect(res[0].text).toBe(newPost1.text);
+        done();
+      });
     });
   });
 });

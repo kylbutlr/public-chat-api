8378975afb0d44db776419d55cc64b8a5d00cf99
Kyle Butler <kylbutlr@gmail.com>
2019-03-22 11:10:12 -0700

    heroku

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const { Client } = require('pg');
 const App = require('./app');
-
 const client = new Client({
-  user: 'postgres',
-  password: 'pass',
-  database: 'public-chat',
+  host: 'ec2-54-83-61-142.compute-1.amazonaws.com',
+  database: 'd9k74lmko8dj3q',
+  user: 'mzwqmxtmxprlyi',
+  password: '2d338c2194cc17e8c4d6e2077f1660b193544b831b811f734c77f224cd1ff2c3',
+  port: 5432,
+  ssl: true
 });
-
 client.connect();
 const app = App(client);
-const port = 3000;
-console.log('Listening on port: '+port);
-app.listen(port);
+app.listen(process.env.PORT || 3000);

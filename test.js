148513bd44521304c8543b016f54f839bf523d08
Kyle Butler <kylbutlr@gmail.com>
2019-03-29 12:32:21 -0700

    fixed tests

--- test.js
+++ test.js
@@ -8,6 +8,30 @@ let client;
 
 const userModel = UserModel(db);
 
+const newUser1 = {
+  username: 'testUser3',
+  password: 'pass',
+};
+const newUser2 = {
+  username: 'testUser4',
+  password: 'pass',
+};
+const newPost1 = {
+  text: 'test entry 3',
+  created: new Date(),
+  user_id: 1,
+};
+const newPost2 = {
+  text: 'test entry 4',
+  created: new Date(),
+  user_id: 1,
+};
+const badPost = {
+  text: 'bad entry',
+  created: new Date(),
+  user_id: -1,
+};
+
 beforeAll(() => {
   client = new Client({
     user: 'postgres',
@@ -18,22 +42,92 @@ beforeAll(() => {
   db = DB(client);
   app = App(client);
 });
+
 afterAll(() => {
   client.end();
 });
 
-describe('posts', () => {
-  const newPost1 = {
-    text: 'test entry 3',
-    created: new Date(),
-    user_id: 1,
-  };
+describe('USERS', () => {
+  describe('GET /users', () => {
+    it('should get from /users', done => {
+      request(app)
+        .get('/users')
+        .expect(200, done);
+    });
+    it('should call db.getUsers', done => {
+      db.getUsers((err, res) => {
+        if (err) throw err;
+        expect(res);
+        done();
+      });
+    });
+  });
 
+  describe('POST /register', () => {
+    it('should post to /register', done => {
+      request(app)
+        .post('/register')
+        .send(newUser1)
+        .expect(201, done);
+    });
+    it('should use userModel and register', done => {
+      userModel.hashPass(newUser2.password, hashedPass => {
+        db.register(newUser2.username, hashedPass, (err, res) => {
+          if (err) throw err;
+          expect(res[0].id).toBe(4);
+          expect(res[0].username).toBe(newUser2.username);
+          done();
+        });
+      });
+    });
+    it('should refuse registering a duplicate username', done => {
+      userModel.hashPass(newUser1.password, hashedPass => {
+        db.register(newUser1.username, hashedPass, (err, res) => {
+          expect(err.code === 23505);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('POST /login', () => {
+    it('should post to /login', done => {
+      request(app)
+        .post('/login')
+        .send(newUser1)
+        .expect(200, done);
+    });
+    it('should use userModel and login', done => {
+      db.login(newUser1.username, (err, res) => {
+        if (err) throw err;
+        expect(res);
+        userModel.checkPass(newUser1.password, res[0].password, passMatch => {
+          expect(passMatch).toBe(true);
+        });
+        done();
+      });
+    });
+    it('should return unauthorized for wrong password', done => {
+      db.login(newUser1.username, (err, res) => {
+        if (err) throw err;
+        expect(res);
+        userModel.checkPass('wrongPass', res[0].password, passMatch => {
+          expect(passMatch).toBe(false);
+        });
+        done();
+      });
+    });
+  });
+});
+
+describe('POSTS', () => {
   describe('GET /posts', () => {
-    it('should return all from /posts', done => {
+    it('should get from /posts', done => {
       request(app)
         .get('/posts')
-        .expect(200);
+        .expect(200, done);
+    });
+    it('should call db.getPosts', done => {
       db.getPosts((err, res) => {
         if (err) throw err;
         expect(res).toHaveLength(2);
@@ -45,11 +139,14 @@ describe('posts', () => {
   });
 
   describe('POST /posts', () => {
-    it('should post to /posts', done => {
+    /* THIS NEEDS TO BE AUTHORIZED TO PASS */
+    /*it('should post to /posts', done => {
       request(app)
         .post('/posts')
         .send(newPost1)
-        .expect(201);
+        .expect(201, done);
+    });*/
+    it('should call db.createPost', done => {
       db.createPost(newPost1.text, newPost1.created, newPost1.user_id, (err, res) => {
         if (err) throw err;
         expect(res[0].user_id).toBe(1);
@@ -57,13 +154,13 @@ describe('posts', () => {
         done();
       });
     });
-  });
-
-  describe('GET /posts', () => {
-    it('should return all from /posts', done => {
+    it('should fail when unauthorized', done => {
       request(app)
-        .get('/posts')
-        .expect(200);
+        .post('/posts')
+        .send(badPost)
+        .expect(401, done);
+    });
+    it('should return 3 from /posts', done => {
       db.getPosts((err, res) => {
         if (err) throw err;
         expect(res).toHaveLength(3);
@@ -75,10 +172,13 @@ describe('posts', () => {
   });
 
   describe('DELETE /posts', () => {
-    it('should delete a post from /posts', done => {
+    /* THIS NEEDS TO BE AUTHORIZED TO PASS */
+    /*it('should delete from /posts', done => {
       request(app)
         .delete('/posts/1')
-        .expect(204);
+        .expect(204, done);
+    });*/
+    it('should call db.deletePost', done => {
       db.deletePost(1, done);
     });
     it('should fail when invalid post', done => {
@@ -91,13 +191,7 @@ describe('posts', () => {
         .delete('/posts/2')
         .expect(401, done);
     });
-  });
-
-  describe('GET /posts', () => {
-    it('should return all from /posts', done => {
-      request(app)
-        .get('/posts')
-        .expect(200);
+    it('should return 2 from /posts', done => {
       db.getPosts((err, res) => {
         if (err) throw err;
         expect(res).toHaveLength(2);
@@ -109,82 +203,10 @@ describe('posts', () => {
   });
 });
 
-describe('users', () => {
-  const newUser1 = {
-    username: 'testUser3',
-    password: 'pass',
-  };
-
-  describe('GET /users', () => {
-    it('should return all from /users', done => {
-      request(app)
-        .get('/users')
-        .expect(200);
-      db.getPosts((err, res) => {
-        if (err) throw err;
-        expect(res).toHaveLength(2);
-        done();
-      });
-    });
-  });
-
-  describe('POST /register', () => {
-    it('should register a user to /users', done => {
-      request(app)
-        .post('/register')
-        .send(newUser1)
-        .expect(201);
-      userModel.hashPass(newUser1.password, hashedPass => {
-        db.register(newUser1.username, hashedPass, (err, res) => {
-          if (err) throw err;
-          expect(res[0].id).toBe(3);
-          expect(res[0].username).toBe(newUser1.username);
-          done();
-        });
-      });
-    });
-    it('should refuse registering a duplicate username', done => {
-      request(app)
-        .post('/register')
-        .send(newUser1)
-        .expect(201);
-      userModel.hashPass(newUser1.password, hashedPass => {
-        db.register(newUser1.username, hashedPass, (err, res) => {
-          expect(err.code === 23505);
-          done();
-        });
-      });
-    });
-  });
-
-  describe('POST /login', () => {
-    it('should login a user to /users', done => {
-      request(app)
-        .post('/login')
-        .send(newUser1)
-        .expect(201);
-      db.login(newUser1.username, (err, res) => {
-        if (err) throw err;
-        expect(res);
-        userModel.checkPass(newUser1.password, res[0].password, passMatch => {
-          expect(passMatch).toBe(true);
-        });
-        done();
-      });
-    });
-    it('should return unauthorized for wrong password', done => {
-      request(app)
-        .post('/login')
-        .send(newUser1)
-        .expect(201);
-      db.login(newUser1.username, (err, res) => {
-        if (err) throw err;
-        expect(res);
-        userModel.checkPass('wrongPass', res[0].password, passMatch => {
-          expect(passMatch).toBe(false);
-        });
-        done();
-      });
-    });
+describe('404', () => {
+  it('should 404', done => {
+    request(app)
+      .get('/nothing')
+      .expect(404, done);
   });
 });
